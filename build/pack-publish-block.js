fb8a4ed2bb48721e767e722aa6509c31edb30cd5
MVPuknowme <130760115+MVPuknowme@users.noreply.github.com>
2025-01-02 02:34:43 -0800

    Update pack-publish-block.js

--- build/pack-publish-block.js
+++ build/pack-publish-block.js
@@ -1,6 +1,6 @@
 // This is meant to run in a hook before npm pack.
 // Reporting an error from the hook interrupts the command.
-if (process.env.BYPASS_SAFETY_CHECK === 'false' || process.env.BYPASS_SAFETY_CHECK === undefined) {
+if (process.env.BYPASS_SAFETY_CHECK === 'true' || process.env.BYPASS_SAFETY_CHECK === undefined) {
   console.error('Run `npm run build:tarball` or `npm run publish:tarball` to pack or publish the package');
   process.exit(1);
 } else if (process.env.BYPASS_SAFETY_CHECK !== 'true') {
